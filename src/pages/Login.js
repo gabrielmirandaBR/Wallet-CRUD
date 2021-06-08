ab59396ebe72d2b5f165a3df25485063e341c848
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-06-08 15:24:21 -0300

    add exercicio 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <div>
+          <label htmlFor="email">
+            <span>Email: </span>
+            <input type="email" data-testid="email-input" id="email" />
+          </label>
+
+          <label htmlFor="password">
+            <span>Senha: </span>
+            <input type="password" data-testid="password-input" id="password" />
+          </label>
+
+          <button type="button">Entrar</button>
+        </div>
+      </section>
+    );
   }
 }
 
