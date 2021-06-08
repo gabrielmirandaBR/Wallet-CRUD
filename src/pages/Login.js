cebd197ec6df24196c85c78594d0350aeaf06b22
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-06-08 16:48:42 -0300

    add exercicio 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,88 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+      redirect: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addLogin = this.addLogin.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+
+    const { email, password } = this.state;
+    const arrEmail = email.split('');
+    const numberOfPassword = 4;
+
+    if (arrEmail.includes('@') === true && arrEmail.includes('.') === true
+    && password.length > numberOfPassword) {
+      this.setState({
+        disabled: false,
+      });
+    }
+  }
+
+  addLogin() {
+    this.setState({
+      redirect: true,
+    });
+  }
+
   render() {
+    const { email, password, disabled, redirect } = this.state;
+    if (!disabled && redirect) {
+      return <Redirect to="/carteira" />;
+    }
+
     return (
       <section>
         <div>
           <label htmlFor="email">
             <span>Email: </span>
-            <input type="email" data-testid="email-input" id="email" />
+            <input
+              name="email"
+              type="email"
+              value={ email }
+              onChange={ this.handleChange }
+              data-testid="email-input"
+              placeholder="alguem@alguem.com"
+              id="email"
+            />
           </label>
 
           <label htmlFor="password">
             <span>Senha: </span>
-            <input type="password" data-testid="password-input" id="password" />
+            <input
+              name="password"
+              type="password"
+              value={ password }
+              onChange={ this.handleChange }
+              data-testid="password-input"
+              placeholder="******"
+              id="password"
+            />
           </label>
 
-          <button type="button">Entrar</button>
+          <button
+            type="button"
+            id="btn-enter"
+            disabled={ disabled }
+            onClick={ this.addLogin }
+          >
+            Entrar
+
+          </button>
         </div>
       </section>
     );
