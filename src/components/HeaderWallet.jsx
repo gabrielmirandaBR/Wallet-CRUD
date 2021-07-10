a39d57d9b4c2a3fbbfa85d6feb4523edc9a89071
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-10 17:17:00 -0300

    finaliza estilizacao Header

--- src/components/HeaderWallet.jsx
+++ src/components/HeaderWallet.jsx
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import '../styles/HeaderWallet.css';
+
 class HeaderWallet extends Component {
   render() {
     const { userEmail, valueTotal } = this.props;
     return (
-      <header>
+      <header className="header">
         <div>
+          <img src="https://img.icons8.com/windows/32/000000/email-open.png" alt="email icon" width="30px" />
+          {' '}
           <span data-testid="email-field">{userEmail}</span>
         </div>
         <div>
+          <img src="https://img.icons8.com/ios/50/000000/money--v1.png" alt="money icon" width="30px" />
+          {' '}
           <span
             data-testid="total-field"
+            className="header_total"
           >
             {`R$ ${parseFloat(valueTotal).toFixed(2)}`}
           </span>
