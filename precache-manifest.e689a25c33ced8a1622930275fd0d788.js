d95b4000667ce42bf80f78f18cf99a46c7857790
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-10 20:22:36 -0300

    Updates

---
+++ precache-manifest.e689a25c33ced8a1622930275fd0d788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f51d2fd5ac0aa33075b3a6423dd2ff",
+    "url": "/Wallet-CRUD/index.html"
+  },
+  {
+    "revision": "84b7f41ed1d1b2a363da",
+    "url": "/Wallet-CRUD/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "db4baa44bf1ba31994b0",
+    "url": "/Wallet-CRUD/static/css/main.623b6138.chunk.css"
+  },
+  {
+    "revision": "84b7f41ed1d1b2a363da",
+    "url": "/Wallet-CRUD/static/js/2.1de5c34e.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/Wallet-CRUD/static/js/2.1de5c34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4baa44bf1ba31994b0",
+    "url": "/Wallet-CRUD/static/js/main.fe0c3a89.chunk.js"
+  },
+  {
+    "revision": "ac5bec142f857f7c563c",
+    "url": "/Wallet-CRUD/static/js/runtime-main.2d708148.js"
+  }
+]);
