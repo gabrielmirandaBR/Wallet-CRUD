5a721bc8b4f1844158d477f556e73b3c57678e8c
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-10 18:25:39 -0300

    implementa tabela somente se tiver um gasto

--- src/components/TableWallet.jsx
+++ src/components/TableWallet.jsx
@@ -46,26 +46,30 @@ class TableWallet extends Component {
   }
 
   render() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.renderTable()}
-        </tbody>
-      </table>
-    );
+    const { currencies } = this.props;
+    if (currencies.length > 0) {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderTable()}
+          </tbody>
+        </table>
+      );
+    }
+    return '';
   }
 }
 
